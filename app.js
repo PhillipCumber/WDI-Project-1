480aa3c13e766fe3f26b40b3fb2746e4b9aa6831
Phillip Cumberlander <cumberlander11@gmail.com>
2018-10-10 13:14:39 -0400

    did a little styling for my header/title section

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 //First Commit to test repo
 
-let how = $('#new').text()
+// let how = $('#new').text()
 // console.log(how)
 
+
+
+    anime({
+        targets: '.title',
+        translateX: 500, duration: 6000,
+        loop: 1
+
+    })
+
+  
