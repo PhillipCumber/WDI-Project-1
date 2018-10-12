8fb47b968403cf370f00da5ad9beab7a4690041f
Phillip Cumberlander <cumberlander11@gmail.com>
2018-10-12 11:58:37 -0400

    decided not to go with a table

--- app.js
+++ app.js
@@ -3,11 +3,12 @@
 //Creating a Jeopardy Game
 
 //step 1 Created a header section for the title of the game. Includes animation and on hover event
+// step 2 Create a table to store all data in for game
+// step 3 Make sure I can click on a box to change the inner text to a question
 
 let title = $('#title')
 // console.log(title)
-
-
+// Title pic to run on load of page, thats why its not in a function
 anime({
 
         targets: '#title',
@@ -20,6 +21,15 @@ anime({
         ],
         loop: 1
 
+})
+
+// About to connect boxes to change inner text to a question
+
+let test = $('.toprow').eq(0)
+
+test.on('click', function () {
+
+    test.text('What is a banana?')
 })
 
   
